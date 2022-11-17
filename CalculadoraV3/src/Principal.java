d95d62874c245a812f8e7c40478fd64d8a17ed12
ALLAN JUNIO DE SOUZA MUNIZ <allan_muniz@estudante.sesisenai.org.br>
2022-11-16 21:27:40 -0300

    Primeiro projeto git

---
+++ CalculadoraV3/src/Principal.java
@@ -0,0 +1,39 @@
+import javax.swing.JOptionPane;
+
+public class Principal {
+	private static Operacoes oper = new Operacoes();
+
+	public static void main(String[] args) {
+
+		double resultado = 0;
+		int continuar;
+		do {
+			double num1 = EntradaSaida.solicitaNumero("1º");
+			double num2 = EntradaSaida.solicitaNumero("2º");
+			int operacao = EntradaSaida.solicitaOperacao();
+
+			switch (operacao) {
+			case 1:
+				resultado = oper.calculaSoma(num1, num2);
+				break;
+			case 2:
+				resultado = oper.calculaSubtracao(num1, num2);
+				break;
+			case 3:
+				resultado = oper.calculaMultiplicacao(num1, num2);
+				break;
+			case 4:
+
+				resultado = oper.calculaDivisao(num1, num2);
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "Operação inválida! A calculadora será encer rada!");
+				System.exit(0);
+			}
+			EntradaSaida.mostraResultado(resultado, operacao);
+			continuar = Integer.parseInt(JOptionPane.showInputDialog("Deseja continuar calculando? 1- Sim | 2- não"));
+
+		} while (continuar == 1);
+	}
+
+}
