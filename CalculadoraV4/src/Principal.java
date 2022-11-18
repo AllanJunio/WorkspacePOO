70e2db12ced291b8b55391e6f9f63a0e5d65d79f
ALLAN JUNIO DE SOUZA MUNIZ <allan_muniz@estudante.sesisenai.org.br>
2022-11-17 22:18:47 -0300

    restaurando

---
+++ CalculadoraV4/src/Principal.java
@@ -0,0 +1,49 @@
+import javax.swing.JOptionPane;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		double resultado = 0;
+		int continuar;
+		do {
+			double num1 = EntradaSaida.solicitaNumero("1º");
+			double num2 = EntradaSaida.solicitaNumero("2º");
+			int operacao = EntradaSaida.solicitaOperacao();
+
+			switch (operacao) {
+			case 1:
+				Soma soma = new Soma();
+				soma.setNum1(num1);
+				soma.setNum2(num2);
+				resultado = soma.calculaSoma();
+				break;
+			case 2:
+				Subtracao sub = new Subtracao();
+				sub.setNum1(num1);
+				sub.setNum2(num2);
+				resultado = sub.calculaSubtracao();
+				break;
+			case 3:
+				Multiplicacao multi = new Multiplicacao();
+				multi.setNum1(num1);
+				multi.setNum2(num2);
+				resultado = multi.calculaMultiplicacao();
+				break;
+			case 4:
+				Divisao divi = new Divisao();
+				divi.setNum1(num1);
+				divi.setNum2(num2);
+				resultado = divi.calculaDivisao();
+				break;
+			default:
+				JOptionPane.showMessageDialog(null, "Operação inválida! A calculadora será encer rada!");
+				System.exit(0);
+			}
+			EntradaSaida.mostraResultado(resultado, operacao);
+			continuar = Integer.parseInt(JOptionPane.showInputDialog("Deseja continuar calculando? 1- Sim | 2- não"));
+
+		} while (continuar == 1);
+	}
+
+}
